73351dcde52e0da7445dec754be6f49a6c79f4b3
Abdallah Sayed <abdallahsayed3355@gmail.com>
2023-05-02 19:12:05 +0300

    create ViewAllCreatorsFragment ,ViewAllCreatorsAdapter , and its layout

--- app/src/main/java/com/example/marvelverse/app/ui/viewAll/MainViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/viewAll/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.marvelverse.app.ui.viewAll
 
 import androidx.lifecycle.ViewModel
 import com.example.marvelverse.data.repositories.MarvelRepository
+import com.example.marvelverse.domain.entities.main.Creator
 import com.example.marvelverse.domain.entities.main.Story
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
@@ -12,6 +13,11 @@ class MainViewModel:ViewModel() {
 
     fun storiesData(): Single<List<Story>> =
        repository.searchStories()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+    fun creatorsData(): Single<List<Creator>> =
+        repository.searchCreators()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
 
--- app/src/main/java/com/example/marvelverse/app/ui/viewAll/bindingAdapters.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/viewAll/bindingAdapters.kt
@@ -3,6 +3,7 @@ package com.example.marvelverse.app.ui.viewAll
 import android.annotation.SuppressLint
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.marvelverse.domain.entities.main.Creator
 import com.example.marvelverse.domain.entities.main.Story
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
@@ -10,13 +11,28 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 
 @SuppressLint("CheckResult")
 @BindingAdapter( value=["app:items"])
-fun setRecyclerItems(view:RecyclerView, items:Single<MutableList<Story>>?){
+fun setRecyclerStoriesItems(view:RecyclerView, items:Single<MutableList<Story>>?){
     items?.
     subscribeOn(Schedulers.io())?.
     observeOn(AndroidSchedulers.mainThread())?.
     subscribe({
         val adapter=ViewAllStoriesAdapter()
          view.adapter= adapter
+        adapter.submitList(it)
+    },{
+
+    })
+}
+
+@SuppressLint("CheckResult")
+@BindingAdapter( value=["app:items"])
+fun setRecyclerCreatorsItems(view:RecyclerView, items:Single<MutableList<Creator>>?){
+    items?.
+    subscribeOn(Schedulers.io())?.
+    observeOn(AndroidSchedulers.mainThread())?.
+    subscribe({
+        val adapter=ViewAllCreatorsAdapter()
+        view.adapter= adapter
         adapter.submitList(it)
     },{
 
