4a0c6577206c2432f070e5285c6c5dabd9072963
Abdallah Sayed <abdallahsayed3355@gmail.com>
2023-05-02 14:08:54 +0300

    add logic to the  view all stories adapter and fix some issues

--- app/src/main/java/com/example/marvelverse/app/ui/viewAll/ViewAllStoriesAdapter.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/viewAll/ViewAllStoriesAdapter.kt
@@ -1,6 +1,41 @@
 package com.example.marvelverse.app.ui.viewAll
 
-class ViewAllStoriesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marvelverse.R
+import com.example.marvelverse.databinding.ItemInViewAllStoriesBinding
+import com.example.marvelverse.domain.entities.main.Story
 
+class ViewAllStoriesAdapter(private val list:Story):
+    ListAdapter<Story,ViewAllStoriesAdapter.ViewAllStoriesHolder>(StoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewAllStoriesHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_in_view_all_stories,parent,false)
+        return ViewAllStoriesHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewAllStoriesHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.item=item
+
+    }
+
+    class ViewAllStoriesHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val binding=ItemInViewAllStoriesBinding.bind(itemView)
+
+    }
+
+    class StoryDiffCallback : DiffUtil.ItemCallback<Story>() {
+        override fun areItemsTheSame(oldItem: Story, newItem: Story): Boolean {
+            return oldItem.id == newItem.id
+        }
+        override fun areContentsTheSame(oldItem: Story, newItem: Story): Boolean {
+            return areItemsTheSame(oldItem, newItem) && oldItem.title == newItem.title
+        }
+    }
 
 }
