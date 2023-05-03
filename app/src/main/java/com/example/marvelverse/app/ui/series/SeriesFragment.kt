9faae859b90d68b7c183cd0b6c9d0bd656f82775
Mahmoud <Elboray>
2023-05-03 16:42:36 +0300

    update fragment and ViewModel

--- app/src/main/java/com/example/marvelverse/app/ui/series/SeriesFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/series/SeriesFragment.kt
@@ -2,12 +2,20 @@ package com.example.marvelverse.app.ui.series
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.example.marvelverse.app.ui.abstracts.BaseFragment
 import com.example.marvelverse.databinding.FragmentSeriesBinding
 
 class SeriesFragment : BaseFragment<FragmentSeriesBinding>(FragmentSeriesBinding::inflate) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val seriesViewModel: ComicsViewModel = ViewModelProvider(this)[ComicsViewModel::class.java]
+        binding.lifecycleOwner=this
+        binding.viewModel=seriesViewModel
+        var comicsAdapter:ComicsAdapter= ComicsAdapter()
+//        comicsAdapter.submitList()
+
     }
 
 }
