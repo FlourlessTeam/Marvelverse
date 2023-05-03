9b46a9d456660b049b989278c73f488f7f97e879
Youssef Abdullah <youssef.abdallah2015@hotmail.com>
2023-05-03 17:59:49 +0300

    move bindingAdapters to FragmentsBindingAdapters.kt

--- app/src/main/java/com/example/marvelverse/app/ui/bindingAdapters/FragmentsBindingAdapters.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/bindingAdapters/FragmentsBindingAdapters.kt
@@ -1,11 +1,21 @@
 package com.example.marvelverse.app.ui.bindingAdapters
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.marvelverse.DataState
+import com.example.marvelverse.app.ui.characters.CharactersAdapter
 import com.example.marvelverse.app.ui.creators.MoreCreatorsAdapter
+import com.example.marvelverse.app.ui.series.SeriesAdapter
 import com.example.marvelverse.app.ui.stories.MoreStoriesAdapter
+import com.example.marvelverse.domain.entities.main.Character
 import com.example.marvelverse.domain.entities.main.Creator
+import com.example.marvelverse.domain.entities.main.Series
 import com.example.marvelverse.domain.entities.main.Story
+import com.example.marvelverse.domain.entities.wrappers.Thumbnail
 
 
 @BindingAdapter( value=["app:items"])
@@ -20,4 +30,53 @@ fun setRecyclerCreatorsItems(view:RecyclerView, items:MutableList<Creator>?){
     val adapter= MoreCreatorsAdapter()
     view.adapter= adapter
     adapter.submitList(items)
+}
+
+@BindingAdapter(value = ["thumbnail"], requireAll = true)
+fun ImageView.bindImageUrl(thumbnail: Thumbnail?) {
+    thumbnail?.let {
+        val imageUrlSecure = thumbnail.path.replace("http", "https")
+        val validUrl = "$imageUrlSecure.${thumbnail.extension}"
+        Glide.with(this)
+            .load(validUrl)
+            .into(this)
+    }
+}
+
+
+@BindingAdapter("bindCharactersList")
+fun <T> RecyclerView.bindCharactersList(dataState: DataState<T>?) {
+    dataState.let {
+        if(dataState is DataState.Success) {
+            (adapter as CharactersAdapter).submitList((dataState.data as List<Character>?) ?: emptyList())
+        }
+    }
+}
+
+@BindingAdapter("bindSeriesList")
+fun <T> RecyclerView.bindSeriesList(dataState: DataState<T>?) {
+    if(dataState is DataState.Success) {
+        (adapter as SeriesAdapter).submitList((dataState.data as List<Series>?) ?: emptyList())
+    }}
+
+@BindingAdapter("showIfLoading")
+fun <T> ProgressBar.showIfLoading(dataState: DataState<T>?) {
+    dataState?.let {
+        visibility = if (dataState is DataState.Loading) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+}
+
+@BindingAdapter("showIfError")
+fun <T> ImageView.showIfError(dataState: DataState<T>?) {
+    dataState?.let {
+        visibility = if (dataState is DataState.Error) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
 }
