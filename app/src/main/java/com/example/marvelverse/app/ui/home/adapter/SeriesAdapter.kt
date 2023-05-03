5600517728a206bdf97d9bde4ca4002195c37435
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-05-03 16:52:25 +0300

    update series adapter

--- app/src/main/java/com/example/marvelverse/app/ui/home/adapter/SeriesAdapter.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/home/adapter/SeriesAdapter.kt
@@ -5,12 +5,6 @@ import com.example.marvelverse.domain.entities.main.Series
 import com.example.nestedrecyclerview.ui.base.BaseAdapter
 import com.example.nestedrecyclerview.ui.base.BaseInteractionListener
 
-class SeriesAdapter(val seriesList: List<Series>,private val listener: OnSeriesClickListener): BaseAdapter<Series>(seriesList,listener) {
+class SeriesAdapter(val seriesList: List<Series>,private val listener: BaseInteractionListener): BaseAdapter<Series>(seriesList,listener) {
   override val layoutID: Int= R.layout.item_series_card
-}
-interface OnSeriesClickListener: BaseInteractionListener {
-  fun onSeriesClick(series: Series)
-}
-interface OnViewAllSeriesClickListener: BaseInteractionListener {
-  fun onViewAllSeriesClick()
 }
