df00ce822071f6903147fa38cd8aa05c097a74ac
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-05-03 16:54:07 +0300

    update view model

--- app/src/main/java/com/example/marvelverse/app/ui/home/MainViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/home/MainViewModel.kt
@@ -1,107 +1,45 @@
 package com.example.marvelverse.app.ui.home
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.marvelverse.data.repositories.MarvelRepository
 import com.example.marvelverse.domain.entities.main.Character
 import com.example.marvelverse.domain.entities.main.Comic
-import com.example.marvelverse.domain.entities.main.Creator
 import com.example.marvelverse.domain.entities.main.Event
 import com.example.marvelverse.domain.entities.main.Series
 import com.example.marvelverse.domain.entities.main.Story
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainViewModel : ViewModel() {
 
-    val characters: MutableLiveData<List<Character>> = MutableLiveData()
-    val comics: MutableLiveData<List<Comic>> = MutableLiveData()
-    val stories: MutableLiveData<List<Story>> = MutableLiveData()
-    val events: MutableLiveData<List<Event>> = MutableLiveData()
-    val creators: MutableLiveData<List<Creator>> = MutableLiveData()
-    val series: MutableLiveData<List<Series>> = MutableLiveData()
-
-    val errorMessage:MutableLiveData<String> = MutableLiveData()
+    val homeItems: MutableLiveData<List<HomeItem>> = MutableLiveData()
+    val errorMessage: MutableLiveData<String> = MutableLiveData()
 
     init {
-        getRandomCharacters()
-        getRandomComics()
-        getRandomStories()
-        getRandomEvents()
-        getRandomCreators()
-        getRandomSeries()
+        getHomeItems()
     }
     @SuppressLint("CheckResult")
-    fun getRandomCharacters() {
-        MarvelRepository.getRandomCharacters()
+    fun getHomeItems() {
+        Single.zip(
+            MarvelRepository.getRandomCharacters(),
+            MarvelRepository.getRandomComics(),
+            MarvelRepository.getRandomStories(),
+            MarvelRepository.getRandomEvents(),
+            MarvelRepository.getRandomSeries()
+        ) { characters: List<Character>, comics: List<Comic>, stories: List<Story>, events: List<Event>, series: List<Series> ->
+            listOf(
+                HomeItem.CharactersItem(characters),
+                HomeItem.ComicsItem(comics),
+                HomeItem.StoriesItem(stories),
+                HomeItem.EventsItem(events),
+                HomeItem.SeriesItem(series)
+            )
+        }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                characters.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getRandomComics() {
-        MarvelRepository.getRandomComics()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                comics.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getRandomStories() {
-        MarvelRepository.getRandomStories()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                stories.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getRandomEvents() {
-        MarvelRepository.getRandomEvents()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                events.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getRandomCreators() {
-        MarvelRepository.getRandomCreators()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                creators.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
-    }
-
-    @SuppressLint("CheckResult")
-    fun getRandomSeries() {
-        MarvelRepository.getRandomSeries()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                series.postValue(it)
-            }, {
-                errorMessage.postValue(it.message)
-            })
+            .subscribe({ homeItems.postValue(it) }, { errorMessage.postValue(it.message) })
     }
 }
