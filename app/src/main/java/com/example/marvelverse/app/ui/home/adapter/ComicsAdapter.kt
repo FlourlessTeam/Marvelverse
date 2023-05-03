41934ffd53ff3af73fb7f828f23bea7ce7b2d38e
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-05-03 14:01:36 +0300

    add comics adapter

---
+++ app/src/main/java/com/example/marvelverse/app/ui/home/adapter/ComicsAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.marvelverse.app.ui.home.adapter
+
+import com.example.marvelverse.R
+import com.example.marvelverse.domain.entities.main.Comic
+import com.example.nestedrecyclerview.ui.base.BaseAdapter
+import com.example.nestedrecyclerview.ui.base.BaseInteractionListener
+
+class ComicsAdapter(val comicList: List<Comic>,private val listener: OnComicClickListener): BaseAdapter<Comic>(comicList,listener) {
+  override val layoutID: Int= R.layout.item_comic_card
+}
+interface OnComicClickListener: BaseInteractionListener {
+  fun onComicClick(comic:Comic)
+}
+interface OnViewAllComicsClickListener: BaseInteractionListener {
+  fun onViewAllComicsClick()
+}
