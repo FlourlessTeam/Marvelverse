e27cc679a163337718f5bd4049652b271ebaea5d
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-05-03 16:52:39 +0300

    update stories adapter

--- app/src/main/java/com/example/marvelverse/app/ui/home/adapter/StoriesAdapter.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/home/adapter/StoriesAdapter.kt
@@ -5,12 +5,6 @@ import com.example.marvelverse.domain.entities.main.Story
 import com.example.nestedrecyclerview.ui.base.BaseAdapter
 import com.example.nestedrecyclerview.ui.base.BaseInteractionListener
 
-class StoriesAdapter (val storiesList: List<Story>, private val listener: OnStoriesClickListener): BaseAdapter<Story>(storiesList,listener) {
+class StoriesAdapter (val storiesList: List<Story>, private val listener: BaseInteractionListener): BaseAdapter<Story>(storiesList,listener) {
   override val layoutID: Int= R.layout.item_stories_card
-}
-interface OnStoriesClickListener: BaseInteractionListener {
-  fun onStoriesClick(stories:Story)
-}
-interface OnViewAllStoriesClickListener: BaseInteractionListener {
-  fun onViewAllStoriesClick()
 }
