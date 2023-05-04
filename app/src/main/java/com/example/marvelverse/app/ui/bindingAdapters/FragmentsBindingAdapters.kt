0d587d6975fcf0c7a4f27695c5279d94bcf272ac
Abdallah Sayed <abdallahsayed3355@gmail.com>
2023-05-04 03:01:58 +0300

    update bindingAdapters

--- app/src/main/java/com/example/marvelverse/app/ui/bindingAdapters/FragmentsBindingAdapters.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/bindingAdapters/FragmentsBindingAdapters.kt
@@ -1,23 +1,33 @@
 package com.example.marvelverse.app.ui.bindingAdapters
 
+import android.net.Uri
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.marvelverse.app.ui.creators.MoreCreatorsAdapter
 import com.example.marvelverse.app.ui.stories.MoreStoriesAdapter
 import com.example.marvelverse.domain.entities.main.Creator
 import com.example.marvelverse.domain.entities.main.Story
+import com.example.marvelverse.domain.entities.wrappers.Thumbnail
 
 
 @BindingAdapter( value=["app:items"])
 fun setRecyclerStoriesItems(view:RecyclerView, items:MutableList<Story>?){
-    val adapter= MoreStoriesAdapter()
-         view.adapter= adapter
-         adapter.submitList(items)
+    if(items!=null){
+        (view.adapter as MoreStoriesAdapter).submitList(items)
+    }
+    else{
+        (view.adapter as MoreStoriesAdapter).submitList(emptyList())
+    }
 }
 
 @BindingAdapter( value=["app:items"])
 fun setRecyclerCreatorsItems(view:RecyclerView, items:MutableList<Creator>?){
-    val adapter= MoreCreatorsAdapter()
-    view.adapter= adapter
-    adapter.submitList(items)
+    if(items!=null){
+        (view.adapter as MoreCreatorsAdapter).submitList(items)
+    }
+    else{
+        (view.adapter as MoreCreatorsAdapter).submitList(emptyList())
+    }
 }
