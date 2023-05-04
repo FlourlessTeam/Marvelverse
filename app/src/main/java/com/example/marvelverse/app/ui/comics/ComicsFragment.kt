f2f352468280b606dab9ecb7b166bc4a364c10bf
أســـــاور <xyzas.7510@gmail.com>
2023-05-04 18:27:08 +0300

    update view model.

--- app/src/main/java/com/example/marvelverse/app/ui/comics/ComicsFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/ComicsFragment.kt
@@ -2,13 +2,68 @@ package com.example.marvelverse.app.ui.comics
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.marvelverse.app.ui.abstracts.BaseFragment
+import com.example.marvelverse.app.ui.home.ParentInteractionListener
+import com.example.marvelverse.app.ui.home.adapter.CharactersAdapter
+import com.example.marvelverse.app.ui.home.adapter.EventAdapter
 import com.example.marvelverse.databinding.FragmentComicsBinding
+import com.example.marvelverse.domain.entities.main.Character
+import com.example.marvelverse.domain.entities.main.Comic
+import com.example.marvelverse.domain.entities.main.Event
+import com.example.marvelverse.domain.entities.main.Series
+import com.example.marvelverse.domain.entities.main.Story
 
 
-class ComicsFragment : BaseFragment<FragmentComicsBinding>(FragmentComicsBinding::inflate) {
+class ComicsFragment : BaseFragment<FragmentComicsBinding>(FragmentComicsBinding::inflate) ,
+    ParentInteractionListener {
+    private val viewModel: ComicsViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        binding.titleRecycler.adapter= CharactersAdapter(listOf(),this)
+        binding.title2Recycler.adapter= EventAdapter(listOf(),this)
     }
 
+    override fun onCharacterClick(character: Character) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEventClick(event: Event) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onComicClick(comic: Comic) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStoriesClick(stories: Story) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSeriesClick(series: Series) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewAllCharactersClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewAllEventsClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewAllComicsClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewAllStoriesClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewAllSeriesClick() {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/marvelverse/app/ui/comics/ComicsViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/ComicsViewModel.kt
@@ -1,7 +1,63 @@
 package com.example.marvelverse.app.ui.comics
 
+import android.provider.CalendarContract.Events
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.marvelverse.DataState
+import com.example.marvelverse.data.repositories.MarvelRepository
+import com.example.marvelverse.domain.entities.main.Character
+import com.example.marvelverse.domain.entities.main.Comic
+import com.example.marvelverse.domain.entities.main.Event
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class ComicsViewModel : ViewModel(){
 
+class ComicsViewModel : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    private var _character = MutableLiveData<DataState<Character>>()
+    private var _event= MutableLiveData<DataState<Event>>()
+    val character: LiveData<DataState<Character>> get() = _character
+    val event: LiveData<DataState<Event>> get() =_event
+//    init {
+//      getCharacter()
+//    }
+
+    private fun getCharacter(url:String) {
+        _character.postValue(DataState.Loading)
+        MarvelRepository.getCharactersByUrl(url)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    _character.postValue(DataState.Success(it))
+                },
+                {
+                    _character.postValue(DataState.Error(it))
+                }).addTo(compositeDisposable)
+    }
+    private fun getEvent(url:String) {
+        _event.postValue(DataState.Loading)
+        MarvelRepository.getEventsByUrl(url)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                  _event.postValue(DataState.Success(it))
+                },
+                {
+                    _event.postValue(DataState.Error(it))
+                }).addTo(compositeDisposable)
+    }
+
+    private fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
+        compositeDisposable.add(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
