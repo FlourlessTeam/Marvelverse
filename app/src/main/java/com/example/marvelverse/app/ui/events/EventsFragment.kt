d1f81ed7a4c18b257dcd7ecaabbdf621fe6a5fa4
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-05-04 09:45:07 +0300

    delete characters adapter in event layout

--- app/src/main/java/com/example/marvelverse/app/ui/events/EventsFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/events/EventsFragment.kt
@@ -2,23 +2,22 @@ package com.example.marvelverse.app.ui.events
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.marvelverse.app.ui.abstracts.BaseFragment
-import com.example.marvelverse.app.ui.home.ParentInteractionListener
 import com.example.marvelverse.app.ui.home.adapter.CharactersAdapter
 import com.example.marvelverse.app.ui.home.adapter.ComicsAdapter
 import com.example.marvelverse.app.ui.home.adapter.SeriesAdapter
+import com.example.marvelverse.app.ui.home.interfaces.CharacterInteractionListener
+import com.example.marvelverse.app.ui.home.interfaces.ComicInteractionListener
+import com.example.marvelverse.app.ui.home.interfaces.SeriesInteractionListener
 import com.example.marvelverse.databinding.FragmentEventsBinding
 import com.example.marvelverse.domain.entities.main.Character
 import com.example.marvelverse.domain.entities.main.Comic
-import com.example.marvelverse.domain.entities.main.Event
 import com.example.marvelverse.domain.entities.main.Series
-import com.example.marvelverse.domain.entities.main.Story
 
 
 class EventsFragment : BaseFragment<FragmentEventsBinding>(FragmentEventsBinding::inflate),
-	ParentInteractionListener {
+	CharacterInteractionListener, SeriesInteractionListener, ComicInteractionListener {
 
 
 	private val viewModel: EventViewModel by viewModels()
@@ -28,7 +27,7 @@ class EventsFragment : BaseFragment<FragmentEventsBinding>(FragmentEventsBinding
 		//binding.item = event
 		binding.lifecycleOwner = this
 
-		binding.characterAdapter = CharactersAdapter(listOf(), this)
+		binding.eventCharacters.adapter = CharactersAdapter(listOf(), this)
 		binding.eventSeries.adapter = SeriesAdapter(listOf(), this)
 		binding.eventComics.adapter = ComicsAdapter(listOf(), this)
 
@@ -44,43 +43,11 @@ class EventsFragment : BaseFragment<FragmentEventsBinding>(FragmentEventsBinding
 	}
 
 	override fun onCharacterClick(character: Character) {
-		Toast.makeText(requireContext(), character.name, Toast.LENGTH_LONG).show()
-	}
-
-	override fun onEventClick(event: Event) {
-		TODO("Not yet implemented")
 	}
 
 	override fun onComicClick(comic: Comic) {
-		TODO("Not yet implemented")
-	}
-
-	override fun onStoriesClick(stories: Story) {
-		TODO("Not yet implemented")
 	}
 
 	override fun onSeriesClick(series: Series) {
-		TODO("Not yet implemented")
 	}
-
-	override fun onViewAllCharactersClick() {
-		TODO("Not yet implemented")
-	}
-
-	override fun onViewAllEventsClick() {
-		TODO("Not yet implemented")
-	}
-
-	override fun onViewAllComicsClick() {
-		TODO("Not yet implemented")
-	}
-
-	override fun onViewAllStoriesClick() {
-		TODO("Not yet implemented")
-	}
-
-	override fun onViewAllSeriesClick() {
-		TODO("Not yet implemented")
-	}
-
 }
