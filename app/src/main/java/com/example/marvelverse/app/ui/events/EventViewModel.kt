977f2fbaa2f57ed3c1cf96704536ed447de2c045
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-05-04 09:44:08 +0300

    update event viewModel

--- app/src/main/java/com/example/marvelverse/app/ui/events/EventViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/events/EventViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.marvelverse.app.ui.events
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,11 +8,12 @@ import com.example.marvelverse.domain.entities.main.Comic
 import com.example.marvelverse.domain.entities.main.Series
 import com.example.marvelverse.domain.entities.main.Character
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class EventViewModel : ViewModel() {
 
+	private val compositeDisposable by lazy { CompositeDisposable() }
 	private val _character: MutableLiveData<List<Character>?> = MutableLiveData()
 	private val _comics: MutableLiveData<List<Comic>?> = MutableLiveData()
 	private val _series: MutableLiveData<List<Series>?> = MutableLiveData()
@@ -26,35 +25,40 @@ class EventViewModel : ViewModel() {
 	val character: LiveData<List<Character>?>
 		get() = _character
 
-	@SuppressLint("CheckResult")
 	fun getCharacters(url: String) {
-		MarvelRepository.getCharactersByUrl(url).subscribeOn(Schedulers.io())
-			.observeOn(AndroidSchedulers.mainThread())
+		val single = MarvelRepository.getCharactersByUrl(url).subscribeOn(Schedulers.io())
+		val disposable = single.observeOn(AndroidSchedulers.mainThread())
 			.subscribe({
-				Log.i("FFF", it.toString())
 				_character.postValue(it)
 			}, {
-				Log.i("FFF", it.message.toString())
 			})
+
+		compositeDisposable.add(disposable)
 	}
 
-	@SuppressLint("CheckResult")
 	fun getComics(url: String) {
-		MarvelRepository.getComicsByUrl(url).subscribeOn(Schedulers.io())
-			.observeOn(AndroidSchedulers.mainThread())
+		val single = MarvelRepository.getComicsByUrl(url).subscribeOn(Schedulers.io())
+		val disposable = single.observeOn(AndroidSchedulers.mainThread())
 			.subscribe({
 				_comics.postValue(it)
 			}, {
 			})
+
+		compositeDisposable.add(disposable)
 	}
 
-	@SuppressLint("CheckResult")
 	fun getSeries(url: String) {
-		MarvelRepository.getSeriesByUrl(url).subscribeOn(Schedulers.io())
-			.observeOn(AndroidSchedulers.mainThread())
+		val single = MarvelRepository.getSeriesByUrl(url).subscribeOn(Schedulers.io())
+		val disposable = single.observeOn(AndroidSchedulers.mainThread())
 			.subscribe({
 				_series.postValue(it)
 			}, {
 			})
+
+		compositeDisposable.add(disposable)
+	}
+
+	override fun onCleared() {
+		compositeDisposable.clear()
 	}
 }
