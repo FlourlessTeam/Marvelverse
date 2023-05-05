48fbd7bb7cc9cc7b5c829880ed99fe701e9023c1
Garfend <71102453+Garfend@users.noreply.github.com>
2023-05-05 23:30:56 +0300

    finish comic adapter, fragment, view model and xml

--- app/src/main/java/com/example/marvelverse/app/ui/comics/ComicAdapter.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/ComicAdapter.kt
@@ -1,48 +1,14 @@
 package com.example.marvelverse.app.ui.comics
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.marvelverse.R
-import com.example.marvelverse.databinding.ItemComicBinding
+import com.example.marvelverse.app.ui.home.interfaces.ComicInteractionListener
 import com.example.marvelverse.domain.entities.main.Comic
-
-class ComicAdapter: ListAdapter<Comic, ComicAdapter.ViewAllComicAdapter>(EventDiffCallback())  {
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewAllComicAdapter {
-        val view= LayoutInflater.from(parent.context).inflate(R.layout.item_comic,parent,false)
-        return  ViewAllComicAdapter(view)
-    }
-
-    override fun onBindViewHolder(
-        holder:  ViewAllComicAdapter,
-        position: Int) {
-        val currentList = getItem(position)
-        holder.binding.item = currentList
+import com.example.nestedrecyclerview.ui.base.BaseAdapter
 
 
-    }
-    class ViewAllComicAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val binding= ItemComicBinding.bind(itemView)
-
-    }
-    class EventDiffCallback : DiffUtil.ItemCallback<Comic>() {
-        override fun areItemsTheSame(oldItem: Comic, newItem: Comic): Boolean {
-            return oldItem.id == newItem.id
-
-        }
-
-        override fun areContentsTheSame(oldItem: Comic, newItem: Comic): Boolean {
-            return areItemsTheSame(oldItem, newItem) && oldItem.title == newItem.title
-        }
-
+    class ComicAdapter(listener: ComicInteractionListener) :
+        BaseAdapter<Comic>(listener) {
+        override val layoutID: Int = R.layout.item_comic
     }
 
 
-}
 
--- app/src/main/java/com/example/marvelverse/app/ui/comics/ComicViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/ComicViewModel.kt
@@ -3,28 +3,58 @@ package com.example.marvelverse.app.ui.comics
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.marvelverse.DataState
+import com.example.marvelverse.app.ui.home.interfaces.ComicInteractionListener
 import com.example.marvelverse.data.repositories.MarvelRepository
 import com.example.marvelverse.domain.entities.main.Comic
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class ComicViewModel: ViewModel() {
-    private val repository = MarvelRepository
-    private val _comicsData = MutableLiveData<MutableList<Comic>>()
-    val comicData: LiveData<MutableList<Comic>>
-        get() = _comicsData
+class ComicViewModel: ViewModel(), ComicInteractionListener {
     private val compositeDisposable = CompositeDisposable()
 
-    fun getComics() {
-        val disposable = repository.searchComics().subscribeOn(Schedulers.io())
+    private var _Comic = MutableLiveData<DataState<Comic>>()
+    val comic: LiveData<DataState<Comic>> get() = _Comic
+
+    private val _comicEvent=MutableLiveData<ComicEvent>()
+    val comicEvent:LiveData<ComicEvent> get() = _comicEvent
+
+    init {
+        getComic()
+    }
+
+    private fun getComic() {
+        _Comic.postValue(DataState.Loading)
+        MarvelRepository.searchComics()
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ _comicsData.postValue(it as MutableList<Comic>?) }, {})
-        compositeDisposable.add(disposable)
+            .subscribe(
+                {
+                    _Comic.postValue(DataState.Success(it))
+                },
+                {
+                    _Comic.postValue(DataState.Error(it))
+                }
+            ).addTo(compositeDisposable)
+    }
+
+
+    private fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
+        compositeDisposable.add(this)
+    }
+
+    override fun onComicClick(comic: Comic) {
+        _comicEvent.postValue(ComicEvent.ClickComicEvent(comic))
+    }
+    fun backToHome(){
+        _comicEvent.postValue(ComicEvent.BackToHome)
     }
 
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
     }
+
 }
