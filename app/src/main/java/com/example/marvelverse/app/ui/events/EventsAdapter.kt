9bd50eac3a92faabccceccb50e48eebe3a0bed3e
Garfend <71102453+Garfend@users.noreply.github.com>
2023-05-05 23:34:01 +0300

    finish event adapter, fragment, view model and xml

--- app/src/main/java/com/example/marvelverse/app/ui/events/EventsAdapter.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/events/EventsAdapter.kt
@@ -1,45 +1,12 @@
 package com.example.marvelverse.app.ui.events
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+
 import com.example.marvelverse.R
-import com.example.marvelverse.databinding.ItemEventBinding
+import com.example.marvelverse.app.ui.home.interfaces.EventInteractionListener
 import com.example.marvelverse.domain.entities.main.Event
+import com.example.nestedrecyclerview.ui.base.BaseAdapter
 
-class EventsAdapter: ListAdapter<Event, EventsAdapter.ViewAllEventHolder>(EventDiffCallback())  {
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewAllEventHolder {
-
-        val view= LayoutInflater.from(parent.context).inflate(R.layout.item_event,parent,false)
-        return ViewAllEventHolder(view)
-    }
-    override fun onBindViewHolder(
-        holder: ViewAllEventHolder,
-        position: Int) {
-        val currentList = getItem(position)
-        holder.binding.item = currentList
-    }
-    class ViewAllEventHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-            val binding= ItemEventBinding.bind(itemView)
-
-    }
-    class EventDiffCallback : DiffUtil.ItemCallback<Event>() {
-        override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
-            return areItemsTheSame(oldItem, newItem) && oldItem.title == newItem.title
-        }
-
-    }
-
-
+class EventsAdapter(listener: EventInteractionListener) :
+    BaseAdapter<Event>(listener) {
+    override val layoutID: Int = R.layout.item_event
 }
 
--- app/src/main/java/com/example/marvelverse/app/ui/events/EventsFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/events/EventsFragment.kt
@@ -1,22 +1,39 @@
 package com.example.marvelverse.app.ui.events
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.marvelverse.app.ui.abstracts.BaseFragment
 import com.example.marvelverse.databinding.FragmentEventsBinding
+import com.example.marvelverse.domain.entities.main.Event
 
 
 class EventsFragment : BaseFragment<FragmentEventsBinding>(FragmentEventsBinding::inflate) {
 
-    private val viewModel: EventsViewModel by viewModels()
-
+    private val eventViewModel: EventsViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-        viewModel.getEvent()
+        val adapter = EventsAdapter(eventViewModel)
+        binding.eventRecyclerView.adapter = adapter
+        binding.viewModel = eventViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        eventViewModel.eventsEvent.observe(viewLifecycleOwner) {
+            it?.let {
+                handleEvent(it)
+            }
+        }
+    }
+    fun handleEvent(event: EventsEvent) {
+        when (event) {
+            is EventsEvent.ClickEventsEvent ->navigateToDetails(event.event)
+            EventsEvent.BackToHome -> BackToHome()
+        }
+    }
+    fun navigateToDetails(event: Event) {
+        Log.d("ComicFragment", "ClickComicEvent $event")
+    }
+    fun BackToHome() {
+        Log.d("ComicFragment", "BackToHome")
     }
 }
