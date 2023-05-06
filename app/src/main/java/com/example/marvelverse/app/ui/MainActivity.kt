61d44b73909a1a96713055f475b07ed802130eea
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-05-06 21:08:06 +0200

    refactor views

--- app/src/main/java/com/example/marvelverse/app/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/MainActivity.kt
@@ -2,26 +2,38 @@ package com.example.marvelverse.app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import com.example.marvelverse.R
 import com.example.marvelverse.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setUpBottomNavigationBar()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragmentContainerView)
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+   private fun setUpBottomNavigationBar(){
+  bottomNavigationView = binding.bottomNavigationBar
+       bottomNavigationView.setOnItemSelectedListener  { item ->
+           when (item.itemId) {
+               R.id.navigation_home -> {
 
-//    override fun onBackPressed() {
-//        val navController = findNavController(R.id.fragmentContainerView)
-//        if (!navController.popBackStack()) {
-//            super.onBackPressed()
-//        }
-//    }
+                   true
+               }
+               R.id.navigation_search -> {
+
+                   true
+               }
+               R.id.navigation_settings -> {
+                   true
+               }
+               else -> false
+           }
+       }
+   }
+
+
 }
