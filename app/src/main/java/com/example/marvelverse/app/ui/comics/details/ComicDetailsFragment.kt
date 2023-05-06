015dddbb32f836a37a86951c1a90ce9ad8abde35
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2023-05-06 16:37:58 +0300

    fix comic details

--- app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicDetailsFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicDetailsFragment.kt
@@ -12,34 +12,46 @@ import com.example.marvelverse.domain.entities.main.Character
 import com.example.marvelverse.domain.entities.main.Event
 
 class ComicDetailsFragment :
-    BaseFragment<FragmentComicDetailsBinding>(FragmentComicDetailsBinding::inflate) {
-    private val viewModel: ComicsDetailsViewModel by viewModels()
-    private val args: ComicDetailsFragmentArgs by navArgs()
+	BaseFragment<FragmentComicDetailsBinding>(FragmentComicDetailsBinding::inflate) {
+	private val viewModel: ComicsDetailsViewModel by viewModels()
+	private val args: ComicDetailsFragmentArgs by navArgs()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-        binding.titleRecycler.adapter = CharactersAdapter(viewModel)
-        binding.title2Recycler.adapter = EventAdapter(viewModel)
-        viewModel.getEvent(args.comic.events.collectionURI)
-        viewModel.getCharacter(args.comic.characters.collectionURI)
-        viewModel.comicsDetailsEvent.observe(viewLifecycleOwner) {
-            when (it) {
-                is ComicDetailsEvents.ClickCharacterEvent -> navigateToCharacterDetails(it.character)
-                is ComicDetailsEvents.ClickEventEvent -> navigateToEventDetails(it.event)
-            }
-        }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.viewModel = viewModel
+		binding.item = args.comic
+		binding.lifecycleOwner = viewLifecycleOwner
+		initRecyclerAdapters()
+		getRecyclerData()
+		observeEvents()
+	}
+
+	private fun initRecyclerAdapters() {
+		binding.characterRecycler.adapter = CharactersAdapter(viewModel)
+		binding.eventRecycler.adapter = EventAdapter(viewModel)
+	}
+
+	private fun getRecyclerData() {
+		viewModel.getEvent(args.comic.events.collectionURI)
+		viewModel.getCharacter(args.comic.characters.collectionURI)
+	}
+
+	private fun observeEvents() {
+		viewModel.comicsDetailsEvent.observe(viewLifecycleOwner) {
+			when (it) {
+				is ComicDetailsEvents.ClickCharacterEvent -> navigateToCharacterDetails(it.character)
+				is ComicDetailsEvents.ClickEventEvent -> navigateToEventDetails(it.event)
+			}
+		}
+	}
 
 
-    }
+	private fun navigateToCharacterDetails(character: Character) {
 
-    private fun navigateToCharacterDetails(character: Character) {
+	}
 
-    }
+	private fun navigateToEventDetails(event: Event) {
 
-    private fun navigateToEventDetails(event: Event) {
-
-    }
+	}
 
 }
