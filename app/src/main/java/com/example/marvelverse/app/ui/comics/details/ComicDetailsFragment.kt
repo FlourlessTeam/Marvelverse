e0ce55ea6b56551bd24ea73a73aea1a72ae6cdd8
أســـــاور <xyzas.7510@gmail.com>
2023-05-06 13:29:57 +0300

    update comic details

--- app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicDetailsFragment.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicDetailsFragment.kt
@@ -3,24 +3,44 @@ package com.example.marvelverse.app.ui.comics.details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.marvelverse.app.ui.abstracts.BaseFragment
 import com.example.marvelverse.app.ui.home.adapter.CharactersAdapter
 import com.example.marvelverse.app.ui.home.adapter.EventAdapter
 import com.example.marvelverse.databinding.FragmentComicDetailsBinding
+import com.example.marvelverse.domain.entities.main.Event
+import com.example.marvelverse.domain.entities.main.Character
+class ComicDetailsFragment :
+    BaseFragment<FragmentComicDetailsBinding>(FragmentComicDetailsBinding::inflate) {
+    private val viewModel: ComicsDetailsViewModel by viewModels()
 
-class ComicDetailsFragment : BaseFragment<FragmentComicDetailsBinding>(FragmentComicDetailsBinding::inflate)
-        {
-        private val viewModel: ComicsDetailsViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        binding.titleRecycler.adapter = CharactersAdapter(viewModel)
+        binding.title2Recycler.adapter = EventAdapter(viewModel)
+        viewModel.getEvent(comic.events.collectionURI)
+        viewModel.getCharacter(comic.characters.collectionURI)
+        viewModel.comicsDetailsEvent.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is ComicDetailsEvents.ClickCharacterEvent -> navigateToCharacterDetails(it.character)
+                is ComicDetailsEvents.ClickEventEvent -> navigateToEventDetails(it.event)
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            binding.viewModel = viewModel
-            binding.lifecycleOwner = this
-            binding.titleRecycler.adapter = CharactersAdapter(listOf(), this)
-            binding.title2Recycler.adapter = EventAdapter(listOf(), this)
-            viewModel.getEvent(comic.events.collectionURI)
-            viewModel.getCharacter(comic.characters.collectionURI)
+            }
 
-        }
+
+        })
+
 
     }
+
+    private fun navigateToCharacterDetails(character: Character) {
+
+    }
+
+    private fun navigateToEventDetails(event: Event) {
+
+    }
+
+}
--- app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicsDetailsViewModel.kt
+++ app/src/main/java/com/example/marvelverse/app/ui/comics/details/ComicsDetailsViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.marvelverse.DataState
+import com.example.marvelverse.app.ui.home.interfaces.CharacterInteractionListener
+import com.example.marvelverse.app.ui.home.interfaces.EventInteractionListener
 import com.example.marvelverse.data.repositories.MarvelRepository
 import com.example.marvelverse.domain.entities.main.Character
 import com.example.marvelverse.domain.entities.main.Event
@@ -13,36 +15,35 @@ import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 
-class ComicsDetailsViewModel : ViewModel() {
+class ComicsDetailsViewModel : ViewModel() , CharacterInteractionListener,
+    EventInteractionListener {
     private val compositeDisposable = CompositeDisposable()
-    private var _character = MutableLiveData<DataState<Character>>()
-    private var _event= MutableLiveData<DataState<Event>>()
-    val character: LiveData<DataState<Character>> get() = _character
-    val event: LiveData<DataState<Event>> get() =_event
+    private var _character = MutableLiveData<List<Character>>()
+    private var _event= MutableLiveData<List<Event>>()
+    private  var _comicsDetailsEvent=MutableLiveData<ComicDetailsEvents>()
+    val character: LiveData<List<Character>> get() = _character
+    val event: LiveData<List<Event>> get() =_event
+    val comicsDetailsEvent:LiveData<ComicDetailsEvents> get() =_comicsDetailsEvent
     fun getCharacter(url:String) {
-        _character.postValue(DataState.Loading)
         MarvelRepository.getCharactersByUrl(url)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
-                    _character.postValue(DataState.Success(it))
+                    _character.postValue(it)
                 },
                 {
-                    _character.postValue(DataState.Error(it))
                 }).addTo(compositeDisposable)
     }
     fun getEvent(url:String) {
-        _event.postValue(DataState.Loading)
         MarvelRepository.getEventsByUrl(url)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
-                  _event.postValue(DataState.Success(it))
+                  _event.postValue(it)
                 },
                 {
-                    _event.postValue(DataState.Error(it))
                 }).addTo(compositeDisposable)
     }
 
@@ -53,5 +54,16 @@ class ComicsDetailsViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
+    }
+
+    override fun onCharacterClick(character: Character) {
+        _comicsDetailsEvent.postValue(ComicDetailsEvents.ClickCharacterEvent(character))
+
+    }
+
+    override fun onEventClick(event: Event) {
+        _comicsDetailsEvent.postValue(ComicDetailsEvents.ClickEventEvent(event))
+
+
     }
 }
